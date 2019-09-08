9cd0392c81f28c7a8762cc072628e90ff7d0c15e
Daniel Doblado <danieldoblado@gmail.com>
2019-09-08 14:15:30 +0200

    Improve postIndex with post organized by dates

--- src/_includes/postsIndex.11ty.js
+++ src/_includes/postsIndex.11ty.js
@@ -1,3 +1,5 @@
+const formatter = new Intl.DateTimeFormat('en', { month: 'short' })
+
 class postIndex {
   data () {
     return {
@@ -5,15 +7,29 @@ class postIndex {
     }
   }
 
-  render ({ collections }) {
+  render ({ collections, content }) {
+    const posts = []
+    collections.post.sort((a, b) => a.date - b.date).forEach(post => {
+      if (!posts[post.date.getFullYear()]) {
+        posts[post.date.getFullYear()] = []
+      }
+      posts[post.date.getFullYear()].push(post)
+    })
+
     return `
-      <ul>
-        ${collections.post.map((post) => `
-          <li>
-            <a href=${post.url}>${post.data.title}</a>
-          </li>
-        `).join('\n')}
-      </ul>
+      ${content}
+      ${Object.keys(posts).sort((a, b) => b - a).map(year => `
+        <h2>${year}</h2>
+        <ul>
+          ${posts[year].map((post) => `
+            <li>
+              <span>${formatter.format(post.date)} ${post.date.getDate()}</span>
+              &nbsp;
+              <a href=${post.url}>${post.data.title}</a>
+            </li>
+          `).join('\n')}
+        </ul>
+      `).join('\n')}
     `
   }
 }
