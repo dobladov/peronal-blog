60897131e8e8cc3bd9a541263abd9827c6fca937
Daniel Doblado <danieldoblado@gmail.com>
2019-09-08 23:53:13 +0200

    Minimize json and html output

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,33 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss")
 const pluginPWA = require("eleventy-plugin-pwa")
+const htmlmin = require("html-minifier")
 const { isProduction } = require('./src/_includes/common')
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPlugin(pluginRss)
-  isProduction && eleventyConfig.addPlugin(pluginPWA)
+
+  if (isProduction) {
+    eleventyConfig.addPlugin(pluginPWA)
+    
+    eleventyConfig.addTransform("htmlmin", (content, outputPath) => {
+      if( outputPath.endsWith(".html") ) {
+        let minified = htmlmin.minify(content, {
+          useShortDoctype: true,
+          removeComments: true,
+          collapseWhitespace: true
+        });
+        return minified
+      }
+      return content
+    })
+
+    eleventyConfig.addTransform("jsonmin", (content, outputPath) => {
+      if( outputPath.endsWith(".json") ) {
+        return JSON.stringify(JSON.parse(content))
+      }
+      return content
+    })
+  }
   
   eleventyConfig.addFilter("inlineHTML", (value) => {
     return value.replace(/\r?\n|\r/g, "")
